3ee156ceb49ec0eddf6185a774f4d090808dea1f
Rohan Agarwal <21052610@kiit.ac.in>
2024-12-07 13:44:32 +0530

    Add files via upload

--- Task-08/server/constants.ts
+++ Task-08/server/constants.ts
@@ -1,2 +1,19 @@
-export const INVALID_USERNAME_ERROR = "Invalid username";
-export const INVALID_PASSWORD_ERROR = "Invalid password";
+import { Credentials } from "../../auth/Credentials";
+import { UserInfo } from "../../auth/UserInfo";
+
+export const VALID_ID = "1";
+
+export const TEST_USER: UserInfo = {
+  id: "cl7qmjh4h0000tothyjqapgj5",
+  roles: ["User"],
+  username: "ofek",
+};
+export const SIGN_TOKEN = "SIGN_TOKEN";
+export const VALID_CREDENTIALS: Credentials = {
+  username: "Valid User",
+  password: "Valid User Password",
+};
+export const INVALID_CREDENTIALS: Credentials = {
+  username: "Invalid User",
+  password: "Invalid User Password",
+};
